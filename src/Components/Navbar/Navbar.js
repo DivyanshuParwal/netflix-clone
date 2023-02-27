fc956ded08973c01fa55ea9dc9e845f61842d233
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2023-02-27 16:45:06 +0530

    add images

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,32 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './Navbar.css';
+import netflix_logo from "../../images/Netflix_logo.png";
+import netflix_avatar from "../../images/Netflix-avatar.png";
+import "./Navbar.css";
 
 const Navbar = () => {
-	const [ show, setShow ] = useState(false);
+  const [show, setShow] = useState(false);
 
-	useEffect(() => {
-		window.addEventListener('scroll', () => {
-			window.scrollY > 100 ? setShow(true) : setShow(false);
-		});
-		return () => window.removeEventListener('scroll');
-	}, []);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 100 ? setShow(true) : setShow(false);
+    });
+    return () => window.removeEventListener("scroll");
+  }, []);
 
-	return (
-		<nav className={`Nav ${show && 'NavBlack'}`}>
-			<img
-				className="NavbarLogo"
-				src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
-				alt="Netflix-Logo"
-			/>
+  return (
+    <nav className={`Nav ${show && "NavBlack"}`}>
+      <img className="NavbarLogo" src={netflix_logo} alt="Netflix-Logo" />
 
-			<img
-				className="NavbarAvatar"
-				src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-				alt="Navbar-Avatar"
-			/>
-		</nav>
-	);
+      <img className="NavbarAvatar" src={netflix_avatar} alt="Navbar-Avatar" />
+    </nav>
+  );
 };
 
 export default Navbar;
